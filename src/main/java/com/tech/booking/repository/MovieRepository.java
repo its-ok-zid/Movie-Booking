a043ed661eb3013a8024d221b15ef4cf42b891fb
zidanali111 <zidanali111@gmail.com>
2025-05-31 17:15:45 +0530

    code cleanup

--- src/main/java/com/tech/booking/repository/MovieRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tech.booking.repository;
-
-import com.tech.booking.model.Movie;
-import com.tech.booking.model.MovieId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, MovieId> {
-}
